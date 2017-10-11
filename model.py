77423869d978c80ac5347f77379bc3baec4a838a
Leandro Gentili (legent-MrG) <leandrogentili@gmail.com>
2017-10-11 22:12:23 +0200

    trying to solve _keras_hisotry issue working with LSTM encoder only

--- model.py
+++ model.py
@@ -2,20 +2,27 @@
 # import keras as K
 import tensorflow as tf
 import numpy as np
-from keras.layers import Input
+import sys
+from keras.models import Model as KerasModel
+from keras.layers import Input, add
 from keras.layers.recurrent import LSTM
 from keras.layers.wrappers import Bidirectional
 from keras.layers.core import Dropout, Flatten, Permute, RepeatVector, Permute, Dense
-from keras.layers.merge import Concatenate, Dot
+from keras.layers.merge import Concatenate, Dot, concatenate, multiply
 from keras.models import Sequential
-from keras.backend import dropout, sigmoid, binary_crossentropy
-from keras.optimizers. import Adam
+from keras.backend import dropout, sigmoid, binary_crossentropy, variable, random_uniform_variable, constant
+from keras.optimizers import Adam
+from keras.initializers import Constant
 
 class Model:
-    def __init__(self, dev_test=False):
-        self.dev_test = dev_test
+    def __init__(self, encoder='lstm', dev_test=False):
+        assert(encoder in ['lstm', 'attentive'])
 
-        #Hyperparams
+        # Input
+        self.dev_test = dev_test
+        self.encoder = encoder
+
+        # Hyperparams
         self.context_length = 10
         self.batch_size = 1000
         self.step_per_epoch = 2000
@@ -30,10 +37,11 @@ class Model:
         self.feature_dim = 50 # dim of feature representation
         self.feature_input_dim = 70
         self.rep_dim = self.lstm_dim * 2 + self.emb_dim # if encoder is not 'averanging'
-        self.rep_dim += self.feature_dim # if --feature
+        # self.rep_dim += self.feature_dim # if --feature
         self.model_metrics = ['accuracy']
 
-        self.model = Sequential()
+        # Not needed anymore! We're using Functional API
+        # self.model = Sequential()
 
         # Placeholders with Tensorflow
         # self.keep_prob = K.backend.placeholder(dtype='float32') # K.backend.placeholder((2, 3), dtype='float32')
@@ -41,70 +49,111 @@ class Model:
         # self.context = [K.backend.placeholder((None, self.emb_dim), dtype='float32') for _ in range((self.context_length * 2) + 1)]
         # self.target = K.backend.placeholder((None, self.target_dim), dtype='float32')
 
-        # Trying to use ---- tensor
-        self.mention_representation = Input(shape=(self.emb_dim,))
-        self.context = Input(shape=(self.emb_dim,))
-        self.target = Input(shape=(self.target_dim,))
-
         # Dropout and split context into L/R
         # Dropout with Keras has a problem... so we have to use tf.nn.dropout!
         # mention_representation_dropout = tf.nn.dropout(mention_representation, keep_prob)
-        self.mention_representation_dropout = dropout(self.mention_representation, self.dropout_)
-        self.left_context = self.context[:self.context_length]
-        self.right_context = self.context[self.context_length + 1:]
 
+        # batch_shape needed when RNN is stateful
+        # self.mention_representation = Input(shape=(self.emb_dim,))
+        # self.left_context = Input(batch_shape=(self.batch_size,self.context_length,self.emb_dim))
+        # self.right_context = Input(batch_shape=(self.batch_size,self.context_length,self.emb_dim))
+        # self.target = Input(batch_shape=(self.batch_size,self.target_dim))
+
+        self.mention_representation = Input(shape=(self.emb_dim,))
+        self.left_context = Input(shape=(self.context_length,self.emb_dim,))
+        self.right_context = Input(shape=(self.context_length,self.emb_dim,))
+        self.target = Input(shape=(self.target_dim,))
+
+        self.mention_representation_dropout = dropout(self.mention_representation, self.dropout_)
+        # self.context = Input(batch_shape=(self.batch_size,self.context_length*2+1,self.emb_dim))
+        # self.left_context = self.context[:self.context_length]
+        # self.right_context = self.context[self.context_length + 1:]
+
+        print self.left_context.shape, self.right_context.shape
         print 'Context placeholder created!'
 
+        if self.encoder == 'lstm':
+            self.L_LSTM, L_state1, L_state2 = LSTM(self.lstm_dim, return_sequences=True, return_state=True, input_shape=self.left_context.shape)(self.left_context)
+            self.R_LSTM, R_state1, R_state2 = LSTM(self.lstm_dim, return_sequences=True, return_state=True, go_backwards=True)(self.right_context)
+            self.context_representation = concatenate([L_state1, R_state1], axis=1)
+
+            print self.L_LSTM
+            print L_state1
+            print L_state2
+            print L_state1.shape
+            print L_state2.shape
+            print self.R_LSTM
+            print R_state1
+            print R_state2
+            print R_state1.shape
+            print R_state2.shape
+
         # if --attentive (LSTM + Attentions)
-        self.left_oneLSTM = LSTM(self.lstm_dim, return_sequences=True, return_state=True, stateful=True)(self.left_context) # stateful=True,
-        self.right_oneLSTM = LSTM(self.lstm_dim, return_sequences=True, return_state=True, stateful=True, go_backwards=True)(self.right_context) # stateful=True,
-        self.left_biLSTM = Bidirectional(self.left_oneLSTM, merge_mode='concat') # (self.left_context)
-        self.right_biLSTM = Bidirectional(self.right_oneLSTM, merge_mode='concat') # (self.right_context)
+        if self.encoder == 'attentive':
+            self.LF_oneLSTM = LSTM(self.lstm_dim, return_sequences=True, stateful=True, input_shape=self.left_context.shape)(self.left_context)
+            self.LB_oneLSTM = LSTM(self.lstm_dim, return_sequences=True, stateful=True, go_backwards=True)(self.left_context)
+            self.L_biLSTM = Concatenate([self.LF_oneLSTM, self.LB_oneLSTM])
+            self.RF_oneLSTM = LSTM(self.lstm_dim, return_sequences=True, stateful=True)(self.right_context)
+            self.RB_oneLSTM = LSTM(self.lstm_dim, return_sequences=True, stateful=True, go_backwards=True)(self.right_context)
+            self.R_biLSTM = Concatenate([self.RF_oneLSTM, self.RB_oneLSTM])
 
-        print 'biLSTM created!'
+            # self.left_biLSTM = Bidirectional(self.left_oneLSTM)
+            # self.right_biLSTM = Bidirectional(self.right_oneLSTM)ù
 
-        # Updating model
-        self.model.add(self.left_biLSTM)
-        self.model.add(Dropout(0.5))
-        self.model.add(self.right_biLSTM)
-        self.model.add(Dropout(0.5))
+            print 'biLSTM created!'
 
-        print 'biLSTM added to model!'
+            # !Dev!
+            if self.dev_test:
+                print 'Got murdered :('
+                sys.exit(1)
 
-        # Dev
-        if self.dev_test:
-            print 'Got murdered :('
-            sys.exit(1)
+            self.LR_biLSTM = Concatenate([self.L_biLSTM, self.R_biLSTM])
 
-        self.merge_biLSTM = merge([self.left_biLSTM, self.right_biLSTM], mode='sum')
+            self.attention = Dense(self.attention_dim, activation='tanh', input_shape=(self.lstm_dim*2,))(self.LR_biLSTM)
+            self.attention = Flatten()(self.attention)
+            self.attention = Activation('softmax')(self.attention)
+            self.attention = RepeatVector(self.lstm_dim)(self.attention)
+            self.attention = Permute([2, 1])(self.attention)
 
-        self.attention = Dense(self.attention_dim, activation='tanh', input_shape=self.lstm_dim*2)(self.merge_biLSTM)
-        self.attention = Flatten()(self.attention)
-        self.attention = Activation('softmax')(self.attention)
-        self.attention = RepeatVector(self.lstm_dim)(self.attention)
-        self.attention = Permute([2, 1])(self.attention)
-
-        self.context_representation = merge([self.activations, self.attention], mode='mul')
+            self.context_representation = merge([self.activations, self.attention], mode='mul')
 
         # Missing --feature part...
         # ...
-        self.representation = Concatenate([self.mention_representation_dropout, self.context_representation], axis=1)
+        self.representation = concatenate([self.mention_representation_dropout, self.context_representation], axis=1)
+        self.representation = Dense(500)(self.representation) # Don't know if it's correct
 
         # Missing --hier part...
         # ...
         self.W = self.create_weight_variable('hier_W', (self.rep_dim, self.target_dim))
-        self.logit = Dot(self.representation, self.W)
 
+        print self.W
+        print self.representation
+
+        # TODO: remove tf.matmul and seek for a Keras implementation
+        self.logit = tf.matmul(self.representation, self.W)
+
+        # Used during prediction phase
         self.distribution = sigmoid(self.logit)
 
-        self.loss_f = np.mean(binary_crossentropy(self.logit, self.target, from_logits=True))
+        # Used during model compilation
+        self.loss_f = tf.reduce_mean(binary_crossentropy(self.logit, self.target, from_logits=True))
         self.optimizer_adam = Adam(lr=self.learning_rate)
+
+        # Creating model
+        # 'Tensor' object has no attribute '_keras_history'
+
+        print self.left_context._keras_history
+        print self.right_context._keras_history
+        print self.mention_representation._keras_history
+        print self.representation._keras_history
+
+        self.model = KerasModel(inputs=[self.left_context, self.right_context, self.mention_representation], outputs=[self.representation])
 
     def set_attention_layer(self, model):
         # Set Attentions...
         return True
 
-    def compile_model(self):
+    def build(self):
         if self.model is not Null:
             self.model.compile(optimizer=self.optimizer_adam, metrics=self.model_metrics, loss=self.loss_f, batch_size=self.batch_size)
 
@@ -118,12 +167,13 @@ class Model:
 
     def create_weight_variable(self, name, shape, pad=True):
         initial = np.random.uniform(-0.01, 0.01, size=shape)
-        # initial = K.random_uniform_variable(shape=shape, -0.01, 0.01)
+        # initial = random_uniform_variable(shape=shape, -0.01, 0.01)
 
         if pad == True:
             initial[0] = np.zeros(shape[1])
 
-        # initial = tf.constant_initializer(initial)
-        initial = tf.contrib.keras.initializers.Constant(initial)
-
-        return K.backend.variable(value=initial)
+        # TODO: insert Constant someway...
+        # initial = variable(initial, name=name, dtype='float32')
+        # initial = tf.constant_initializer(initial)
+        initial = constant(initial, shape=shape, name=name)
+        return initial
--- ner.py
+++ ner.py
@@ -10,23 +10,25 @@ from batcher import Batcher
 ##
 ## Load dicts and datasets
 ##
-dicts, train_dataset, dev_dataset, test_dataset = Loader({
-    'dicts': "data/Wiki/dicts_figer.pkl",
-    'train': "data/Wiki/train_figer.pkl",
-    'dev': "data/Wiki/dev_figer.pkl",
-    'test': "data/Wiki/test_figer.pkl"
-}).get_data()
+# dicts, train_dataset, dev_dataset, test_dataset = Loader({
+#     'dicts': "data/Wiki/dicts_figer.pkl",
+#     'train': "data/Wiki/train_figer.pkl",
+#     'dev': "data/Wiki/dev_figer.pkl",
+#     'test': "data/Wiki/test_figer.pkl"
+# }).get_data()
+#
+# print "train_dataset_size", train_dataset["data"].shape[0]
+# print "dev_dataset_size", dev_dataset["data"].shape[0]
+# print "test_dataset_size", test_dataset["data"].shape[0]
+#
+# #batch_size : 1000, context_length : 10
+# train_batcher = Batcher(train_dataset["storage"],train_dataset["data"],1000,10,dicts["id2vec"])
+# dev_batcher = Batcher(dev_dataset["storage"],dev_dataset["data"],dev_dataset["data"].shape[0],10,dicts["id2vec"])
+# test_batcher = Batcher(test_dataset["storage"],test_dataset["data"],test_dataset["data"].shape[0],10,dicts["id2vec"])
 
-print "train_dataset_size", train_dataset["data"].shape[0]
-print "dev_dataset_size", dev_dataset["data"].shape[0]
-print "test_dataset_size", test_dataset["data"].shape[0]
-
-# batch_size : 1000, context_length : 10
-train_batcher = Batcher(train_dataset["storage"],train_dataset["data"],1000,10,dicts["id2vec"])
-dev_batcher = Batcher(dev_dataset["storage"],dev_dataset["data"],dev_dataset["data"].shape[0],10,dicts["id2vec"])
-test_batcher = Batcher(test_dataset["storage"],test_dataset["data"],test_dataset["data"].shape[0],10,dicts["id2vec"])
-
-model = Model(dev_test=True)
+# model = Model(dev_test=True).get_model()
+model = Model('lstm').get_model()
+model.build()
 
 # model.train()
 
